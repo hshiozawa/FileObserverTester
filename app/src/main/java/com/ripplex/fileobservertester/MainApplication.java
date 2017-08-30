bcc3e2e2439653cdaf9d113d0089dc1878a099a6
Hajime Shiozawa <shiozawa@ripplex.com>
2017-08-30 10:00:05 +0900

    implemented original application class

---
+++ app/src/main/java/com/ripplex/fileobservertester/MainApplication.java
@@ -0,0 +1,14 @@
+package com.ripplex.fileobservertester;
+
+import android.app.Application;
+import android.util.Log;
+
+public class MainApplication extends Application {
+    private static final String TAG = MainApplication.class.getSimpleName();
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Log.d(TAG, "onCreate()");
+    }
+}
