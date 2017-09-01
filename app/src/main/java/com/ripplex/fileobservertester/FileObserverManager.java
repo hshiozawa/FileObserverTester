5bb4db373ac1eecf7320f7619c0ebde7f472b1d8
Hajime Shiozawa <shiozawa@ripplex.com>
2017-09-01 10:46:24 +0900

    implement file exists

--- app/src/main/java/com/ripplex/fileobservertester/FileObserverManager.java
+++ app/src/main/java/com/ripplex/fileobservertester/FileObserverManager.java
@@ -3,18 +3,19 @@ package com.ripplex.fileobservertester;
 import android.os.FileObserver;
 import android.util.Log;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 public class FileObserverManager {
     private static final String TAG = FileObserverManager.class.getSimpleName();
 
-    private static final int TARGET_EVENTS = FileObserver.DELETE;
+    private static final int TARGET_EVENTS = FileObserver.CREATE | FileObserver.DELETE | FileObserver.MOVED_TO;
     // for debug
-    // private static final int TARGET_EVENTS = FileObserver.ALL_EVENTS;
+    //private static final int TARGET_EVENTS = FileObserver.ALL_EVENTS;
 
     private static final String[] TARGETS = new String[]{
-            "/storage/emulated/0/DCIM/Camera/"
+            "/storage/emulated/0/DCIM/Camera",
     };
 
     private List<FileObserver> observers = new ArrayList<>();
@@ -23,13 +24,13 @@ public class FileObserverManager {
         endWatch();
 
         Log.i(TAG, "startWatch");
-        for (String target : TARGETS) {
+        for (final String target : TARGETS) {
             Log.i(TAG, "-- " + target);
 
             FileObserver observer = new FileObserver(target, TARGET_EVENTS) {
                 @Override
                 public void onEvent(int event, String path) {
-                    Log.i(TAG, "onEvent event=" + event + ",path=" + path);
+                    onFileEvent(event, path, target);
                 }
             };
             observer.startWatching();
@@ -45,5 +46,38 @@ public class FileObserverManager {
         }
 
         observers.clear();
+    }
+
+    private void onFileEvent(int event, String path, String basePath) {
+        Log.i(TAG, "onFileEvent event=" + event + ",path=" + path);
+        switch (event) {
+            case FileObserver.CREATE:
+                Log.i(TAG, "-- CREATE");
+                checkFileExists(path, basePath, false);
+                break;
+            case FileObserver.MOVED_TO:
+                Log.i(TAG, "-- MOVED_TO");
+                checkFileExists(path, basePath, false);
+                break;
+            case FileObserver.DELETE:
+                Log.i(TAG, "-- DELETE");
+                checkFileExists(path, basePath, true);
+                break;
+            default:
+                break;
+        }
+    }
+
+    private static void checkFileExists(String path, String basePath, boolean loopUntilDeleted) {
+        File file = new File(basePath + "/" + path);
+        boolean exists;
+        do {
+            exists = file.exists();
+            if (exists) {
+                Log.w(TAG, "-- File exists. fullPath=" + file.getAbsolutePath());
+            } else {
+                Log.i(TAG, "-- File doesn't exists. fullPath=" + file.getAbsolutePath());
+            }
+        } while (loopUntilDeleted && exists);
     }
 }
